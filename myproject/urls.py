8a3452e3d950832f8b9193c75839371865587e7d
SEONGHEON HA <hshs8094@naver.com>
2022-05-30 21:23:03 +0900

    first commit

--- myproject/urls.py
+++ myproject/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 import myapp.views
+from accounts import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',myapp.views.home,name="home"), 
     path('accounts/', include('accounts.urls')),
+    path('login/', views.login, name='login'),
+    path('logout/', views.logout, name='logout'),
 ]
