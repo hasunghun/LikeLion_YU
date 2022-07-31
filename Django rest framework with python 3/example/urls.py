3bdbf51f3a2e588faac34bdfaa53956b4d1f93ab
hasunghun <hshs8094@naver.com>
2022-08-01 01:07:20 +0900

    first commit

---
+++ Django rest framework with python 3/example/urls.py
@@ -0,0 +1,22 @@
+from django.urls import path, include
+from .views import HelloAPI, booksAPI, bookAPI, BookAPI, BooksAPI, BooksAPIMixins, BookAPIMixins, BooksAPIGenerics, BookAPIGenerics
+
+urlpatterns = [
+    path('hello/', HelloAPI),
+    path('fbv/books/', booksAPI),
+    path('fbv/book/<int:bid>/', bookAPI),
+    path('cbv/books/', BooksAPI.as_view()),
+    path('cbv/book/<int:bid>/', BookAPI.as_view()),
+    path("mixin/books/", BooksAPIMixins.as_view()),
+    path("mixin/book/<int:bid>/", BookAPIMixins.as_view()),
+    path("generics/books/", BooksAPIGenerics.as_view()),
+    path("generics/book/<int:bid>/", BookAPIGenerics.as_view())
+]
+
+from rest_framework import routers
+from .views import BookViewSet
+
+router = routers.SimpleRouter()
+router.register('books', BookViewSet)
+
+urlpatterns = router.urls
